66a556708bd73a7bc04e8bb79ef543dd00309d16
Loren-France <14529846@itisrossi.vi.it>
2025-12-19 17:22:05 +0100

    Definitivo

--- Script/script.js
+++ Script/script.js
@@ -1,5 +1,4 @@
-let prima=true;
-let seconda=false;
+
 let immaginiDisponibili = [
     "Image/1coppia.png",
     "Image/2coppia.png",
@@ -20,88 +19,125 @@ let immaginiDisponibili = [
     "Image/17coppia.png",
     "Image/18coppia.png"
 ];
-let older;
-let output="";
+
+let carteImages = [];
+let firstIndex = null;
+let secondIndex = null;
+let lock = false;
+let matchesFound = 0;
+let output = "";
 
 function start() {
-let diff=document.getElementById("difficoltà").value;
-let carte;
-let N=0;
+    let diff = document.getElementById("difficoltà").value;
+    let N = diff == "1" ? 4 : diff == "2" ? 6 : 8;
 
-N= diff=="1" ? 4
-    : diff=="2" ? 6
-    : 8;
+    firstIndex = null;
+    secondIndex = null;
+    lock = false;
+    matchesFound = 0;
+    output = "";
 
-    if(N==4) {
-        carte=[[],[],[],[]];
+    const total = N * N;
+    let imgs = [];
+    for (let i = 0; i < total / 2; i++) {
+        imgs.push(immaginiDisponibili[i]);
+        imgs.push(immaginiDisponibili[i]);
     }
-    else if(N==6) {
-        carte=[[],[],[],[],[],[]];
-    }
-    else{
-        carte=[[],[],[],[],[],[],[],[]];
-    }
+    carteImages = shuffle(imgs);
 
-    crea(carte,N);
+    crea(N);
 
-document.getElementById("output").innerHTML = output;
+    document.getElementById("output").innerHTML = output;
 }
 
-function crea(carte,N) {
-    let tabella="";
-    let c=1;
+function crea(N) {
+    let tabella = "";
+    let c = 1;
 
-let immagini = [];
-    for (let i = 0; i < (N*N)/2; i++) {
-        immagini.push(immaginiDisponibili[i]);
-        immagini.push(immaginiDisponibili[i]); 
-    }
-    immagini = shuffle(immagini);
-
-    tabella+="<tbody>";
-    for(let i=0;i<N;i++) {
-    tabella+="<tr>";
-    for(let j=0;j<N;j++){
-        carte[i][j] = "<button onclick='cliccato(" + c +","+ N + ")' class='carte' id='carta" + c + "'><img src='Image/memory_back.png' alt='back'></button>";
-        tabella+="<td>"+carte[i][j]+"</td>";
-        c++;
-    }
-    tabella+="</tr>";
-}
-    tabella+="</tbody>";
-
-document.getElementById("schema").innerHTML = tabella;
-}
-
-function cliccato(c,N) {
-let randomCard1=Math.floor(Math.random()*(N*N))+1;
-let randomCard2=Math.floor(Math.random()*(N*N))+1;
-
-    if(prima){
-        document.getElementById("carta"+c).disabled = true;
-        document.getElementById("carta"+c).innerHTML = "<img src='"+immagini+"' alt='coppia'>";
-        older="carta"+c;
-        seconda=true;
-        prima=false;
-    }else if(seconda){
-        document.getElementById("carta"+c).innerHTML = "<img src='Image/"+randomCard2+"coppia.png' alt='coppia'>";
-        if(randomCard1==randomCard2){
-            output+="Hai trovato una coppia!";
-        } else{
-            output+="Non è una coppia, riprova!";
-        for(let i=1;i<=N*N;i++){
-            document.getElementById("carta"+i).disabled = true;
+    tabella += "<tbody>";
+    for (let i = 0; i < N; i++) {
+        tabella += "<tr>";
+        for (let j = 0; j < N; j++) {
+            tabella += "<td>";
+            tabella += "<button onclick='cliccato(" + c + "," + N + ")' class='carte' id='carta" + c + "' data-index='" + (c - 1) + "'>";
+            tabella += "<img src='Image/memory_back.png' alt='back'>";
+            tabella += "</button>";
+            tabella += "</td>";
+            c++;
         }
-        setTimeout( () => {
-            document.getElementById("carta"+c).innerHTML = "<img src='Image/memory_back.png' alt='back'>";
-            document.getElementById(older).innerHTML = "<img src='Image/memory_back.png' alt='back'>";
-            for(let i=1;i<=N*N;i++){
-                document.getElementById("carta"+i).disabled = false;
+        tabella += "</tr>";
+    }
+    tabella += "</tbody>";
+
+    document.getElementById("schema").innerHTML = tabella;
+}
+
+function cliccato(c, N) {
+    if (lock) return;
+
+    const idx = c - 1;
+    const btn = document.getElementById("carta" + c);
+
+    if (!btn) return;
+
+    if (firstIndex === idx) return;
+
+    btn.innerHTML = "<img src='" + carteImages[idx] + "' alt='coppia'>";
+
+    btn.disabled = true;
+
+    if (firstIndex === null) {
+        firstIndex = idx;
+    } else {
+        secondIndex = idx;
+        lock = true;
+
+        if (carteImages[firstIndex] === carteImages[secondIndex]) {
+            matchesFound++;
+            output += "Hai trovato una coppia!<br>";
+
+            firstIndex = null;
+            secondIndex = null;
+            lock = false;
+
+            if (matchesFound === (N * N) / 2) {
+                output += "<strong>Hai trovato tutte le coppie! Complimenti!</strong><br>";
             }
-        }, 2000);
+            document.getElementById("output").innerHTML = output;
+        } else {
+            output += "Non è una coppia, riprova!<br>";
+            document.getElementById("output").innerHTML = output;
+
+            setTimeout(() => {
+                const id1 = "carta" + (firstIndex + 1);
+                const id2 = "carta" + (secondIndex + 1);
+                const b1 = document.getElementById(id1);
+                const b2 = document.getElementById(id2);
+                if (b1) {
+                    b1.innerHTML = "<img src='Image/memory_back.png' alt='back'>";
+                    b1.disabled = false;
+                }
+                if (b2) {
+                    b2.innerHTML = "<img src='Image/memory_back.png' alt='back'>";
+                    b2.disabled = false;
+                }
+
+                // resetta stati
+                firstIndex = null;
+                secondIndex = null;
+                lock = false;
+            }, 2000);
         }
-        prima=true;
-        seconda=false;
-    }else{}
-document.getElementById("output").innerHTML = output;
+    }
+
+    document.getElementById("output").innerHTML = output;
+}
+
+function shuffle(array) {
+    let a = array.slice();
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
 }
